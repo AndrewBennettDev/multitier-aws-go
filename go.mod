ba8ab24e8accd42b44a6c682d32d546f3c914375
AndrewBennettDev <andrewbennettdev@gmail.com>
2023-11-07 15:46:32 -0500

    initial commit, basic aws setup

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/AndrewBennettDev/multitier-aws-go
+
+go 1.21.0
+
+require (
+	github.com/aws/aws-sdk-go v1.47.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func main() {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
+	if err != nil {
+		log.Fatal("Failed to create AWS session:", err)
+	}
+
+	ec2Svc := ec2.New(sess)
+	ec2Instance, err := ec2Svc.RunInstances(&ec2.RunInstancesInput{
+		ImageId:      aws.String("your_ami"),
+		InstanceType: aws.String("your_instance_type"),
+		MinCount:     aws.Int64(1),
+		MaxCount:     aws.Int64(1),
+	})
+	if err != nil {
+		log.Fatal("Failed to create EC2 instance:", err)
+	}
+	fmt.Println("EC2 Instance ID:", *ec2Instance.Instances[0].InstanceId)
+
+	s3Svc := s3.New(sess)
+	bucketName := "your_bucket"
+	_, err = s3Svc.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(bucketName),
+	})
+	if err != nil {
+		log.Fatal("Failed to create S3 bucket:", err)
+	}
+	fmt.Println("S3 Bucket created:", bucketName)
+
+	dynamoDBSvc := dynamodb.New(sess)
+	tableName := "YourTable"
+	_, err = dynamoDBSvc.CreateTable(&dynamodb.CreateTableInput{
+		TableName: aws.String(tableName),
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("YourPrimaryKey"),
+				KeyType:       aws.String("HASH"),
+			},
+		},
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("YourPrimaryKey"),
+				AttributeType: aws.String("S"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(5),
+			WriteCapacityUnits: aws.Int64(5),
+		},
+	})
+	if err != nil {
+		log.Fatal("Failed to create DynamoDB table:", err)
+	}
+	fmt.Println("DynamoDB Table created:", tableName)
+}
